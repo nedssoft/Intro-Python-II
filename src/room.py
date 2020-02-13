bde1f321e00cb3ae6cbd02ef156bc244ba205856
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-02-13 14:08:18 +0100

    Implement the method to print room items

--- src/room.py
+++ src/room.py
@@ -7,3 +7,14 @@ class Room:
         self.description = description
         self.items = items
 
+    def __str__(self):
+        if (not len(self.items)):
+            return f'There is no item in {self.name} room'
+        else:
+            output = f'{self.name} has the following items\n'
+            item_count = 1
+            for item in self.items:
+                output += f'[{item_count}]: {item}\n'
+                item_count += 1
+            return output
+
